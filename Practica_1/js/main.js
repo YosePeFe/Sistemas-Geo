2fff9e397bb0ca061a82cecf463de9690d94a7d1
Yoseline Pérez Fernández <yose@MacBook-Pro-de-Yoseline.local>
2021-02-22 03:13:39 -0600

    Se agregó la práctica 1

--- Practica_1/js/main.js
+++ Practica_1/js/main.js
@@ -1,7 +1,7 @@
 AOS.init();
 function iniciaMapa() {
     var coordenadas ={
-        lat : 16.861254, lng : -99.875713
+        lat : 40.45326318841044, lng : -3.688317608915776
     }
     var map = new google.maps.Map(document.getElementById('mapa'),
     {
