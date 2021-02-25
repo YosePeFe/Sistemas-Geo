7c1a02282c5fa62ccc0626fd6c6a16d5620ab814
Yoseline Pérez Fernández <yose@MacBook-Pro-de-Yoseline.local>
2021-02-24 20:05:54 -0600

    Se agregó la práctica 2

---
+++ Practica_2/js/main.js
@@ -0,0 +1,11 @@
+var navegador = document.getElementById("navegador");
+
+var datos = navegador.getElementsByTagName("li");
+
+function obtieneDatos() {
+    datos[0].innerHTML = "Nombre del Navegador: " + navigator.appCodeName;
+    datos[1].innerHTML = "Versión del Navegador: " + navigator.appVersion;
+    datos[2].innerHTML = "Estatus de Internet: " + navigator.onLine;
+    datos[3].innerHTML = "Plataforma: " + navigator.platform;
+
+}
