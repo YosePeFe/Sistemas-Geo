84d489f3b90265977212961adee6112bc410f3f2
Yoseline Pérez Fernández <yose@MacBook-Pro-de-Yoseline.local>
2021-03-03 20:36:47 -0600

    Se agregó la práctica 5

--- Practica_5/js/main.js
+++ Practica_5/js/main.js
@@ -1,11 +1,45 @@
 function iniciaMapa() {
+
+    var informacion =  "<h1>Universidad DeLaSalle Bajío AC.</h1>"
+            informacion += "<p><strong>Dirección:</strong> Avenida Universidad 602, Lomas del Campestre, 37150 León, Gto, México.</p>"
+            informacion += "<p><strong>Teléfono:</strong>477 710 8500.</p>"
+
     var propiedades = {
         center: {
-            lat : 40.45326318841044, lng : -3.688317608915776
+            lat: 21.152639, lng: -101.711598
         },
         zoom: 14
     };
 
-    const mapa = document.getElementById("map");
-    const map = new google.maps.Map(mapa,propiedades);   
+    const mapa = document.getElementById("map")
+    const map = new google.maps.Map(mapa,propiedades)
+
+    if(navigator.geolocation){
+
+        navigator.geolocation.getCurrentPosition( position => {
+
+            let posicion = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            }
+
+            let propiedadesMarcador = {
+                position: posicion,
+                map,
+                title: "Marcardor"
+            }
+
+            const marcador = new google.maps.Marker(propiedadesMarcador);
+
+            map.setCenter(posicion)
+
+            const infowindow = new google.maps.InfoWindow({
+                content : informacion
+            })
+
+            marcador.addListener("click", ()=>{
+                infowindow.open(map,marcador);
+            })
+        })
+    }
 }
