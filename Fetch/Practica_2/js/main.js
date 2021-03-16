c430c5a26d20bf4d08e8b52bcba67504ae034e8d
Yoseline Pérez Fernández <yose@MacBook-Pro-de-Yoseline.local>
2021-03-15 21:42:29 -0600

    Se agregaron las prácticas Fetch

---
+++ Fetch/Practica_2/js/main.js
@@ -0,0 +1,23 @@
+var paises = document.getElementById("paises");
+        fetch('https://corona.lmao.ninja/v3/covid-19/countries')
+        .then( function(response){
+
+            response.json().then( function(datos){
+
+                datos.forEach( registro => {
+
+                    console.log(registro.country);
+                    let renglon = document.createElement("div");
+                    renglon.className = "row border bg-light";
+                    paises.appendChild(renglon);
+
+                    let columna = document.createElement("div");
+                    columna.className = "col-12";
+                    renglon.appendChild(columna);
+
+                    let nombre = document.createElement("p");
+                    nombre.textContent = "Número de casos en " + registro.country + ": " + registro.cases
+                    columna.appendChild(nombre);
+                });
+            });
+        });
