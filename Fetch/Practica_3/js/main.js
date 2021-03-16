e2ee24ef318fe77cfca027b4b0a6f9b915a5d7fa
Yoseline Pérez Fernández <yose@MacBook-Pro-de-Yoseline.local>
2021-03-15 19:20:26 -0600

    Se agregaron las prácticas Fetch

---
+++ Fetch/Practica_3/js/main.js
@@ -0,0 +1,43 @@
+function obtieneMarcadores() {
+
+    const markers = [
+        {
+            name: "México",
+            longitude: "-99.1276",
+            latitude: "19.427"
+        }, {
+            name: "Brazil",
+            longitude: "-47.9292",
+            latitude: "-15.7801"
+        }, {
+            name: "Spain",
+            longitude: "-3.70327",
+            latitude: "40.4167"
+        }
+    ];
+    return markers;
+};
+
+function iniciaMapa() {
+
+    var coordenadas = {
+        lat: 0,
+        lng: 0
+    };
+
+    var propiedades = {
+        center: coordenadas,
+        zoom: 2
+    };
+    
+    const map = new google.maps.Map(document.getElementById('map'), propiedades);
+    const marcadores = obtieneMarcadores();
+    console.log(marcadores);
+    for (marcador of marcadores) {
+        let marker = new google.maps.Marker({
+            map: map,
+            position: new google.maps.LatLng(marcador.latitude, marcador.longitude),
+            title: marcador.name
+        });
+    }
+};
